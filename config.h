fd3aeed91dd72e1fbeac10d1f75523f6b345d9f7
Gabriel Dallagnoli <187823668+dallagnoli@users.noreply.github.com>
2025-09-23 14:54:37 -0300

    bump version and update config

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int refreshrate    = 120;      /* refresh rate for smoother window movements */
 static const unsigned int borderpx       = 1;        /* border pixel of windows */
-static const unsigned int snap           = 15;       /* snap pixel */
+static const unsigned int snap           = 16;       /* snap pixel */
 static const unsigned int systraypinning = 0;        /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft  = 0;        /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 5;        /* systray spacing */
@@ -14,18 +14,17 @@ static const int showbar                 = 1;        /* 0 means no bar */
 static const int topbar                  = 1;        /* 0 means bottom bar */
 #define ICONSIZE                         16          /* icon size */
 #define ICONSPACING                      8           /* space between icon and title */
-static const char *fonts[]               = { "MesloLGS Nerd Font Mono:size=16", "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true" };
-static const char normfg[]               = "#d8dee9";
-static const char normbg[]               = "#2e3440";
-static const char normborder[]           = "#3b4252";
-static const char selfg[]                = "#eceff4";
-static const char selbg[]                = "#434c5e";
-static const char selborder[]            = "#434c5e";
+static const char *fonts[]               = { "MesloLGS Nerd Font Mono:size=16", "NotoColorEmoji:pixelsize=16" };
+static const char nord0[]                = "#2e3440";
+static const char nord1[]                = "#3b4252";
+static const char nord2[]                = "#434c5e";
+static const char nord3[]                = "#4c566a";
+static const char nord4[]                = "#d8dee9";
 
 static const char *colors[][3]           = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { normfg,    normbg,    normborder },
-	[SchemeSel]  = { selfg,     selbg,     selborder  },
+	[SchemeNorm] = { nord3,     nord0,     nord1 },
+	[SchemeSel]  = { nord4,     nord0,     nord2 },
 };
 
 static const char *const autostart[] = {
@@ -46,13 +45,10 @@ static const char *tags[] = { "", "", "󰊖", "", "" };
 static const Rule rules[] = {
 	/* class             instance    title            tags mask  isfloating  isterminal   noswallow  monitor */
 	{ "Alacritty",       NULL,       NULL,            0,         0,          1,           0,         -1 },
-	{ "Xarchiver",       NULL,       NULL,            0,         1,          0,           0,         -1 },
     { "Lutris",          NULL,       NULL,            0,         1,          0,           0,         -1 },
-    { "steam",           NULL,       NULL,            0,         1,          0,           0,         -1 },
 	{ "Steam",           NULL,       NULL,            0,         1,          0,           0,         -1 },
+    { "steam",           NULL,       NULL,            0,         1,          0,           0,         -1 },
 	{ "zenity",          NULL,       NULL,            0,         1,          0,           0,         -1 },
-	{ "Virt-manager",    NULL,       NULL,            0,         1,          0,           0,         -1 },
-	{ "dolphin-emu",     NULL,       NULL,            0,         1,          0,           0,         -1 },
 	{ "qBittorrent",     NULL,       NULL,            0,         1,          0,           0,         -1 },
 	{ "pavucontrol",     NULL,       NULL,            0,         1,          0,           0,         -1 },
 	{ NULL,              NULL,       "Event Tester",  0,         0,          0,           1,         -1 }, /* xev */
@@ -66,8 +62,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "",      tile },    /* first entry is default */
-	{ "",      NULL },    /* no layout function means floating behavior */
+	{ "",         tile },    /* first entry is default */
+	{ "",         NULL },    /* no layout function means floating behavior */
 };
 
 /* key definitions */
@@ -129,18 +125,13 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_3,                            2)
 	TAGKEYS(                        XK_4,                            3)
 	TAGKEYS(                        XK_5,                            4)
-	{ MODKEY|ShiftMask,             XK_BackSpace,  quit,             {0} },
+	{ MODKEY|ShiftMask,             XK_q,          quit,             {0} },
 };
 
 /* button definitions */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkStatusText,        0,              Button2,        spawn,          SHCMD ("pavucontrol") },
 	{ ClkClientWin,         MODKEY,         Button1,        moveorplace,    {.i = 2} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
